64b13dd501a11cec539587faa99b8b58a22c3c6c
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-02-20 14:06:15 +0530

    ongoing

--- script.js
+++ script.js
@@ -29,17 +29,17 @@ menuItmes.forEach(item => {
 // search messages
 const searchMessage = () => {
     const val = messageSearch.value.toLowerCase();
-    message.forEach(chat =>{
-        let name =  chat.querySelectorAll('h5').textContent.toLowerCase();
-        if(name.indexOf(val) != -1){
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
             chat.style.display = 'flex';
-        }else{
+        } else {
             chat.style.display = "none"
         }
     })
 }
 
-messageSearch.addEventListener('keyup',searchMessage);
+messageSearch.addEventListener('keyup', searchMessage);
 
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)'
@@ -50,3 +50,25 @@ messagesNotification.addEventListener('click', () => {
 })
 
 // theme customization 
+
+const theme = document.querySelector("#theme");
+const themeModal = document.querySelector(".customize-theme")
+// open modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid'
+}
+// close modal 
+const closeThemeModal = (e) => {
+    if (e.target.classList.contains('customize-theme')) {
+        themeModal.style.display = "none"
+    }
+}
+themeModal.addEventListener('click', closeThemeModal)
+
+theme.addEventListener('click', openThemeModal);
+
+// for changing the fonts 
+const fontSizes = document.querySelectorAll(".choose-size")
+fontSizes.forEach(size => {
+    let fontSizes;
+})
